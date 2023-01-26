826f95ee8371a343fe14f5619c88887fda18aaf8
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2023-01-26 14:34:45 +0530

    Update metasploit-termux-fix-2023.sh

--- metasploit-termux-fix-2023.sh
+++ metasploit-termux-fix-2023.sh
@@ -88,7 +88,6 @@ cd $PREFIX/opt
 git clone https://github.com/rapid7/metasploit-framework.git --depth=1
 
 echo
-center "*** Installation..."
 cd $PREFIX/opt/metasploit-framework
 
 gem install bundler
@@ -131,5 +130,6 @@ echo
 # Warning occurs during payload generation
 #sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
 #sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
-echo -e "[${Green}+${clear}] Watch full video to fix ${Blue}LinkHere"
-echo -e "[${Green}+${clear}] You can start msfconsole"
+echo -e "[${Green}+${clear}] Watch full video to fix ${Blue}Link https://youtu.be/KBNXzUlKOwk"
+echo -e "[${Green}+${clear}] You can start ${Yellow}msfconsole ${clear}"
+echo -e "[${Green}+${clear}] Connect db ${Yellow}msfdb init ${clear}"
