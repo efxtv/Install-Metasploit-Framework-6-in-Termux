831b368ee0d90dcc288797c00fb6193a83e26d8f
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2024-07-24 18:54:00 +0530

    Update metasploit-termux-fix-2023.sh

--- metasploit-termux-fix-2023.sh
+++ metasploit-termux-fix-2023.sh
@@ -51,14 +51,12 @@ sleep 1
 echo -e "[${Green}+${clear}] Follow the video shared in ${Yellow}t.me/efxtv ${clear}"
 sleep 1
 cd $HOME
-echo -e "[${Green}+${clear}] Done..."
+echo -e "[${Green}+${clear}] Done"
 bars
 echo
-echo -e "[${Yellow}+${clear}] Please wait..."
+echo -e "[${Green}+${clear}] Please wait"
 sleep 3
-echo "[+] Updating termux..."
-sleep 3
-echo -e "[${Yellow}+${clear}] Don't hit Enter use Y/y to update..."
+echo -e "[${Green}+${clear}] Update termux"
 sleep 3
 clear
 echo -e "
@@ -68,75 +66,56 @@ echo -e "
 "
 echo
 sleep 1
-pkg update;pkg upgrade;apt update;apt upgrade
 
-#-----------------------------------------------------------------
-#  Script edited by efx-tv created by |b|y| |G|u|s|h|m|a|z|u|k|o|
-#-----------------------------------------------------------------
-
-echo -e "[${Yellow}+${clear}] Loading..."
-sleep 1
-
-echo -e "[${Yellow}+${clear}] Removing old repository..."
-rm $PREFIX/etc/apt/sources.list.d/* 2>.log.txt
-
-echo -e "[${Yellow}+${clear}] Repository signature..."
-# pkg install -y gnupg
-
-echo -e "[${Yellow}+${clear}] Ruby purge..."
-sleep 1
-apt purge ruby -y
-rm -fr $PREFIX/lib/ruby/gems 2>.log.txt
-
+# Dependencies Installation
+bars
+echo -e "[${Green}+${clear}] Dependencies Installation"
+pkg update -y
 pkg upgrade -y -o Dpkg::Options::="--force-confnew"
-
-echo -e "[${Yellow}+${clear}] Installing missing repository..."
-sleep 1
 pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
-
-python3 -m pip install --upgrade pip
 python3 -m pip install requests
 
-source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
+echo -e "[${Green}+${clear}] Fix ruby BigDecimal"
+# Fix ruby BigDecimal 
+#center "* Fix ruby BigDecimal"
+#source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
 
-rm -rf $PREFIX/opt/metasploit-framework 2>.log.txt
-sleep 3
-rm -rf $HOME/metasploit-framework 2>.log.txt
-mkdir -p $PREFIX/opt 2>.log.txt
-echo
-echo -e "[${Yellow}+${clear}] Downloading Metasploit termux...\c";latestverr
-cd $PREFIX/opt
-git clone https://github.com/rapid7/metasploit-framework.git --depth=1
+echo -e "[${Green}+${clear}] Erase Old Metasploit Folder"
+# Erase Old Metasploit Folder
+bars
+if [ -d "${PREFIX}/opt/metasploit-framework" ]; then
+  rm -rf ${PREFIX}/opt/metasploit-framework
+fi
 
-echo
-cd $PREFIX/opt/metasploit-framework
+echo -e "[${Green}+${clear}] Download Metasploit"
+# Download Metasploit
+bars
+if [ ! -d "${PREFIX}/opt" ]; then
+  mkdir ${PREFIX}/opt
+fi
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1 ${PREFIX}/opt/metasploit-framework
 
+echo -e "[${Green}+${clear}] Install Metasploit"
+# Install Metasploit
+bars
+cd ${PREFIX}/opt/metasploit-framework
 gem install bundler
-declare NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
-#sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+# by overriding cflags nokogiri will install or you can simply declare a void function 
+#  you might have seen this error while installing nokogiri `xmlSetStructuredErrorFunc((void *)rb_error_list, Nokogiri_error_array_pusher);`
+#  solution : void xmlSetStructuredErrorFunc(void *rb_error_list, void *Nokogiri_error_array_pusher); you can set any parameter name 
+#  for sake of simplicity tweaking cflags is better than declaring a void function for every c file
 
-gem install nokogiri -v $NOKOGIRI_VERSION -- --use-system-libraries
-
-# for aarch64 if nokogiri problem persist do this 
-
-bundle config build.nokogiri "--use-system-libraries --with-xml2-include=$PREFIX/include/libxml2"; bundle install
-
+gem install nokogiri -v $NOKOGIRI_VERSION -- --with-cflags="-Wno-implicit-function-declaration -Wno-deprecated-declarations -Wno-incompatible-function-pointer-types" --use-system-libraries
+bundle install
 gem install actionpack
 bundle update activesupport
 bundle update --bundler
 bundle install -j$(nproc --all)
 
-#$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
-# rm ./modules/auxiliary/gather/http_pdf_authors.rb
-rm $PREFIX/bin/msf* 2>.log.txt
-termux-elf-cleaner $PREFIX/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
-echo
-
-# Warning occurs during payload generation
-#sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
-#sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
-sed -i 's#nio4r (2.5.8)#nio4r (2.5.9)#g' $PREFIX/opt/metasploit-framework/Gemfile.lock;sed -i 's#2.1.4#2.1.10#g' $PREFIX/opt/metasploit-framework/Gemfile.lock;cd $PREFIX/opt/metasploit-framework/;bundle
-echo -e "[${Green}+${clear}] Watch full video to fix ${Blue}Link https://youtu.be/zxSN9TfWRLc"
+echo -e "[${Green}+${clear}] Link Metasploit Executables"
+# Link Metasploit Executables
+# echo -e "[${Green}+${clear}] Watch full video to fix ${Blue}Link https://youtu.be/zxSN9TfWRLc"
 echo -e "[${Green}+${clear}] Join for updates ${Blue}Link https://t.me/efxtv"
 echo -e "[${Green}+${clear}] You can start ${Yellow}msfconsole ${clear}"
 echo -e "[${Green}+${clear}] Connect db ${Yellow}msfdb init ${clear}"
@@ -158,5 +137,5 @@ ln -s $PREFIX/opt/metasploit-framework/msfdb $PREFIX/bin/
 bars
 echo -e "[${Green}+${clear}] Try msfconsole"
 bars
-echo -e "[${Green}+${clear}] Join T.me/efxtv"
+echo -e "[${Green}+${clear}] Join T.me/efxtv for more"
 bars
